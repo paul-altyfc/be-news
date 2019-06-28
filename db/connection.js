5f5ff69a6decb81c081ab6e36a44c26fb4efae38
paul-altyfc <paul.m.dewey@gmail.com>
2019-06-28 08:46:50 +0100

    Heroku connections file updated

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,11 @@
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const dbConfig = require('../knexfile.js');
+// const dbConfig = require('../knexfile.js');
+
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
 
 // console.log(dbConfig);
 
